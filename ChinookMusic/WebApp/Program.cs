ae5947e4f149bb8a794e2ced027de1f86db6c23a
Chris <jbana1@nait.ca>
2022-11-02 20:40:24 -0600

    PART 1: Add the extension method requires a parameter options.UseSqlServer(xxxx) where the xxxx is the conenction string variable

--- ChinookMusic/WebApp/Program.cs
+++ ChinookMusic/WebApp/Program.cs
@@ -5,9 +5,30 @@ using WebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Supplied database connection due to the fact that we created this web app to use Individual Accounts
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add another GetConnectionString to reference our DB connectionString
+
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+// Given for the DB connection to DefaultConnection string
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Code the DBConnection to the application DB context for ChinookDB
+// The implementation of the connect and registration of theChinookSystem services will be done
+//      in the ChinookSystem class library
+// So to accomplish this task, we will be using an "extension method"
+// The extension method will extend the IServiceCollection class
+// The extension method requires a parameter options.UseSqlServer(xxxx)
+//      where the xxxx is the conenction string variable
+
+/*builder.Services.ChinookSystemBackendDependencies(options =>
+    options.UseSqlServer(connectionStringChinook));*/
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
