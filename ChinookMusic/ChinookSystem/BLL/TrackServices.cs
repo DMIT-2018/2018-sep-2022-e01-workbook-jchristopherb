05e0a6a83839241c7aa22ae4d621b6bc2d79083f
Chris <jbana1@nait.ca>
2022-11-04 20:34:04 -0600

    Adding the TrackSelection method from linqpad to the Services class under BLL

--- ChinookMusic/ChinookSystem/BLL/TrackServices.cs
+++ ChinookMusic/ChinookSystem/BLL/TrackServices.cs
@@ -1,4 +1,5 @@
 ﻿using ChinookSystem.DAL;
+using ChinookSystem.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,35 @@ namespace ChinookSystem.BLL
         }
 
 
+        #endregion
+
+        #region Queries
+        public List<TrackSelection> Track_FetchTracksBy(string searcharg, string searchby)
+        {
+            if (string.IsNullOrWhiteSpace(searcharg))
+            {
+                throw new ArgumentNullException("No search value submitted");
+            }
+            if (string.IsNullOrWhiteSpace(searchby))
+            {
+                throw new ArgumentNullException("No search style submitted");
+            }
+            IEnumerable<TrackSelection> results = _context.Tracks
+                                        .Where(x => (x.Album.Artist.Name.Contains(searcharg) &&
+                                                    searchby.Equals("Artist")) ||
+                                                    (x.Album.Title.Contains(searcharg) &&
+                                                    searchby.Equals("Album")))
+                                        .Select(x => new TrackSelection
+                                        {
+                                            TrackId = x.TrackId,
+                                            SongName = x.Name,
+                                            AlbumTitle = x.Album.Title,
+                                            ArtistName = x.Album.Artist.Name,
+                                            Milliseconds = x.Milliseconds,
+                                            Price = x.UnitPrice
+                                        });
+            return results.ToList();
+        }
         #endregion
     }
 }
